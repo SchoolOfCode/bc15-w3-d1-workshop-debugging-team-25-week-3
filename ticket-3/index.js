9b2af0282445802b39be381e8d2a2f3755f46830
fpole <137444215+fpole@users.noreply.github.com>
2023-09-18 15:01:36 +0100

    started ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,11 @@
-function countNumberOfNinesInNumber(number) {
+function countNumberOfNinesInNumber([number]) {
   let count = 0;
 
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if ("9" === digit) {
+    if (9 === digit) {
       count++;
     }
   }
-
   return count;
 }
