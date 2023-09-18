a5d31260a7513c61c87c916d1dfba18c91b18681
Sara Thampi <saraannthampi@gmail.com>
2023-09-18 15:59:15 +0100

    ticket 5

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -3,7 +3,7 @@ function countNumberOfNinesInNumber(number) {
 
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if ("9" === digit) {
+    if (9 === digit) {
       count++;
     }
   }
